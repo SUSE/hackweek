1d70974ea41247f33be61e9e8c0f47270ec7d5ce
Thomas Schmidt <tschmidt@suse.de>
2013-02-27 16:28:02 +0100

    add fade-in and -out for menu

--- js/hackweek.js
+++ js/hackweek.js
@@ -1,9 +1,50 @@
 $(function () {
 
+    $("#what-is-link").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#what-is").offset().top
+        }, 1000);
+    })
+
     $("#projects-link").click(function () {
         $('html, body').animate({
             scrollTop: $("#projects").offset().top
         }, 1000);
     })
 
-})
+    $("#agenda-link").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#agenda").offset().top
+        }, 1000);
+    })
+
+    $("#join-link").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#join").offset().top
+        }, 1000);
+    })
+
+    $("#where-link").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#where").offset().top
+        }, 1000);
+    })
+
+
+    $(window).scroll(function () {
+
+        if (!$('#navigation').is(":visible") && $(window).scrollTop() >= $('#what-is').height()/2) {
+            $('#navigation').fadeIn()
+        }
+
+        if ($('#navigation').is(":visible") && $(window).scrollTop() < $('#what-is').height()/2) {
+            $('#navigation').fadeOut()
+        }
+
+    })
+
+
+});
+
+
+
