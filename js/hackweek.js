b8f01b8c91d7ea08c5869055e639c3beddd72129
Thomas Schmidt <tschmidt@suse.de>
2013-02-27 18:00:27 +0100

    fade in and out text

--- js/hackweek.js
+++ js/hackweek.js
@@ -43,20 +43,42 @@ $(function () {
         /* hide - unhide navigation */
         if (!$('#navigation').is(":visible") && $(window).scrollTop() >= $('#what-is').height()/2) {
             $('#navigation').fadeIn()
+            $('#arrow-start').fadeOut()
+
         }
         if ($('#navigation').is(":visible") && $(window).scrollTop() < $('#what-is').height()/2) {
             $('#navigation').fadeOut()
+            $('#arrow-start').fadeIn()
         }
 
         /* start page text animation */
         if ($(window).scrollTop() <= $('#start').height()) {
             var percent_scrolled_out = $(window).scrollTop() / $('#start').height()
-            $("#claim-line1").css({ top: $(window).scrollTop() - percent_scrolled_out * $("#claim-line1").position().top });
-            $("#claim-line2").css({ top: $(window).scrollTop() - percent_scrolled_out * $("#claim-line2").position().top });
-            $("#claim-line3").css({ top: $(window).scrollTop() - percent_scrolled_out * $("#claim-line3").position().top });
+            $("#claim-line1").css({ top: $(window).scrollTop() - (percent_scrolled_out * $("#claim-line1").position().top) / 6 });
+            $("#claim-line2").css({ top: $(window).scrollTop() - (percent_scrolled_out * $("#claim-line2").position().top) / 6 });
+            $("#claim-line3").css({ top: $(window).scrollTop() - (percent_scrolled_out * $("#claim-line3").position().top) / 6 });
 
         }
     })
+
+
+    if ($(window).scrollTop() == 0) {
+        $("#claim-line1").css({ right: $(window).width() })
+        $("#claim-line1").animate({
+            left: 0
+        }, 1000);
+
+        $("#claim-line2").css({ right: $(window).width() })
+        $("#claim-line2").animate({
+            left: 0
+        }, 1000);
+
+        $("#claim-line3").css({ right: $(window).width() })
+        $("#claim-line3").animate({
+            left: 0
+        }, 1000);
+    }
+
 
 
 });
