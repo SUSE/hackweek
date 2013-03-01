43df6c09239f769ebd93c9f2cb48e2b3325ef7dd
Kenneth Wimer <kwwii@suse.de>
2013-03-01 15:22:06 +0100

    added some functions for terminal to show off

--- js/terminal_logic.js
+++ js/terminal_logic.js
@@ -1,10 +1,10 @@
 $(function () {
   var projects = [
     {
-      title: "<span class='welcome-message'>Make a nifty hackweek website<span>"
+      title: "<span class='welcome-message'>Make a nifty hackweek website</span>"
     },
     {
-      title: "<span class='welcome-message'>Do something great<span>"
+      title: "<span class='welcome-message'>Do something great</span>"
     }
   ];
 
@@ -14,4 +14,47 @@ $(function () {
     });
     return titles.join('<br>');
   });
+
+  $.register_command( 'help', function() {
+    return "<span class='welcome-message'>SUSEterm</span>" + '<br>' +
+      "<span class='welcome-message'>ls - List All Projects</span><br>" +
+      "<span class='welcome-message'>eval - Usage eval &lt;any javascript exression&gt;</span><br>" +
+      "<span class='welcome-message'>date - Returns Current Date</span><br>" +
+      "<span class='welcome-message'>cap - Usage cap &lt;string&gt; - Turns the string to upcase</span><br>" +
+      "<span class='welcome-message'>go - Usage go &lt;url&gt; - Sets the browser location to URL</span><br>"
+  });
+
+// COPY AND PASTED FUNCTIONS, REMOVE ON PRODUCTION
+  var command_directory = {
+    'eval': function( tokens ) {
+      tokens.shift();
+      var expression = tokens.join( ' ' );
+      var result = '';
+      try {
+        result = eval( expression );
+      } catch( e ) {
+        result = 'Error: ' + e.message;
+    }
+    return "<span class='welcome-message'>" + result + "</span>";
+  },
+    'date': function( tokens ) {
+      var now = new Date();
+      return "<span class='welcome-message'>" + now.getDate() + '-' +
+      now.getMonth() + '-' +
+      ( 1900 + now.getYear() ) + "</span>"
+    },
+    'cap': function( tokens ) {
+      tokens.shift();
+      return "<span class='welcome-message'>" + tokens.join( ' ' ).toUpperCase() + "</span>";
+    },
+    'go': function( tokens ) {
+      var url = tokens[1];
+      document.location.href = url;
+    }
+  };
+
+  for( var j in command_directory ) {
+    $.register_command( j, command_directory[j] );
+  } 
+
 });
