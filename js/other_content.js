6da1c305235137024789a96667f0bc4f7eee6055
Andre Duffeck <aduffeck@suse.de>
2013-03-15 13:43:49 +0100

    fix "what" content

--- js/other_content.js
+++ js/other_content.js
@@ -3,12 +3,11 @@ var other_content = {
     "What is Hack Week \n\
   ======================= \n\
   * Hackweek is the week where SUSE developers can experiment without limits. \n\
-  * This is an opportunity for SUSE's developers to use their Innovation Time Off<br> \n\
+  * This is an opportunity for SUSE's developers to use their Innovation Time Off \n\
   * and hunker down and work on the projects that catch their fancy. \n\
   \n\
   Hack Week 9 \n\
   ================= \n\
-  ### SUSE LINUX Products GmbH ### \n\
   * Hackweek 9 takes place April 08-12 2013. As usual, SUSE extends the invitation \n\
   * to openSUSE Community contributors. \n\
   ",
