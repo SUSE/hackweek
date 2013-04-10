f8ea81fa6ce1511c870cbaf84b1ed49537916a01
Cornelius Schumacher <cschum@suse.de>
2013-04-10 13:56:11 +0200

    Add newline, so shell doesn't get confused when calling via curl.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,7 +6,7 @@ class ApiController < ApplicationController
     json = request.body.read
     begin
       ProjectImporter.import json
-      render :text => "ok"
+      render :text => "ok\n"
     rescue StandardError => err
       render :text => "Error: #{err}\n", :status => 500
     end    
