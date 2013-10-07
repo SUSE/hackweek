343d556050cb75a7b8c826726220fd7956c50137
Henne Vogelsang <hvogel@opensuse.org>
2013-10-07 10:56:16 +0200

    The gallery is public, add it to deploy as pics come in

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,5 +1,7 @@
 class GalleryController < ApplicationController
 
+skip_before_filter :authenticate_user!, :only => [ :index ]
+
   def index
     path = Rails.root.to_s + '/public/gallery/'
     full = Dir.entries(path)
