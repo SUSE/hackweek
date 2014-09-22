c5460204c6647052806e43c90a9f9f8c4eea552e
Thomas Schmidt <tschmidt@suse.de>
2014-09-22 14:20:46 +0200

    add count helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def bootstrap_class_for flash_type
     case flash_type
       when 'success'
@@ -13,4 +14,9 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def days_to_launch
+    DateTime.new(2014,10,20).mjd - DateTime.now.mjd
+  end
+
 end
