0964c0e0bf80fd99d77e17fa329947d57d4c8dd1
Henne Vogelsang <hvogel@opensuse.org>
2014-10-09 16:26:46 +0200

    Bring back the flash and announcements

--- app/assets/javascripts/responsiveDashboard.js
+++ app/assets/javascripts/responsiveDashboard.js
@@ -5,10 +5,20 @@ $(window).on("load", function(){
 $(window).on("resize", function(){
   layoutResponsive();
 });
+
 function layoutResponsive () {
 	var windowHeight = $(window).height();
-	var homeButtonHeight = $("#home-menu-contact").height(); 
+	var homeButtonHeight = $("#home-menu-contact").height();
+	var flashHeight = $("#flash").height(); 
+        if (flashHeight > 1) {
+          flashHeight = flashHeight + 40 // 35 is the margin
+        }
+	var announcementHeight = $("#announcement").height();
+        if (announcementHeight > 1) {
+          announcementHeight = announcementHeight + 50 // 35 is the margin
+        }
+
 	$(".mainScreen").css({
-		height: windowHeight - homeButtonHeight - 20 + "px" //20 is to give it a padding
+		height: windowHeight - flashHeight - announcementHeight - homeButtonHeight - 20 + "px" //20 is to give it a padding
 	})
 }
