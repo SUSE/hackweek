3346f8f88c8c00dd7c124e95c94386cc505f63fb
Björn Geuken <bgeuken@suse.de>
2014-10-14 16:03:20 +0200

    Add tests for episode model

--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -1,8 +1,15 @@
 # This will guess the User class
 FactoryGirl.define do
   factory :episode do
-    name 1 
+    sequence(:name) { |n| "episode_#{n}" }
     start_date { 1.day.ago }
-    end_date { 7.days.from_now } 
+    end_date { 7.days.from_now }
+
+    factory :active_episode do
+      # ensure there is only one active episode
+      after(:create) do |b|
+        b.active = true
+      end
+    end
   end
 end
---
+++ spec/models/episode_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe Episode do
+
+  describe "active" do
+    it "returns the active episode" do
+      3.times { FactoryGirl.create(:episode) }
+      active_episode = FactoryGirl.create(:active_episode)
+      expect(Episode.active).to eq(active_episode)
+      active_episode.delete
+      expect(Episode.active).to eq(nil)
+    end
+  end
+
+  describe "active=" do
+    it "activates the episode" do
+      episode = FactoryGirl.create(:episode)
+      episode.active = true
+      expect(Episode.active).to eq(episode)
+    end
+
+    it "deactivates all other episodes" do
+      activated_episodes = [
+        FactoryGirl.create(:episode),
+        FactoryGirl.create(:episode)
+      ]
+      activated_episodes.each { |e| e.update_attributes!(:active => true) }
+
+      episode = FactoryGirl.create(:episode)
+      episode.active = true
+      activated_episodes.each do |e|
+        expect(e.reload.active).to eq(false)
+      end
+    end
+  end
+end
