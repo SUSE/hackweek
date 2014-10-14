bb3ef781ba15143043bc1d006c087def901d4b45
Björn Geuken <bgeuken@suse.de>
2014-10-14 11:04:21 +0200

    Allow scrolling of projects by episodes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -13,6 +13,15 @@ describe Project do
       expect(@first.next).to eq(@second)
       expect(@second.next).to eq(@third)
     end
+
+    it "sorts by episode" do
+      create(:episode).active = true
+      episode = create(:episode)
+      @first.episodes << episode
+      @third.episodes << episode
+
+      expect(@first.next(episode)).to eq(@third)
+    end
   end
 
   describe "previous" do
@@ -25,6 +34,15 @@ describe Project do
     it "returns the previous project (sorted by id)" do
       expect(@second.previous).to eq(@first)
       expect(@third.previous).to eq(@second)
+    end
+
+    it "sorts by episodes" do
+      create(:episode).active = true
+      episode = create(:episode)
+      @first.episodes << episode
+      @third.episodes << episode
+
+      expect(@third.previous(episode)).to eq(@first)
     end
   end
 end
