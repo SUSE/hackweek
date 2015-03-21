b99c882be556e92e490fe679da405700c143b88b
Kirill Pimenov <kirill@pimenov.cc>
2015-03-21 16:19:57 +0100

    Final touch

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,16 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# The following is a monkey-patch to allow rails development server to listen on any interface
+# In 4.2, `rails s` starts server on localhost only, which is not suitable for our Vagrant setup
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Host => '0.0.0.0')
+    end
+  end
+end
