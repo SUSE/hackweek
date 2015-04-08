6deff020a316f7b9a428de92ef6b3b7ba99a9872
Henne Vogelsang <hvogel@opensuse.org>
2015-04-08 16:27:46 +0200

    Switch the gallery to the hack week pool on flickr

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,19 +1,5 @@
 class GalleryController < ApplicationController
 skip_before_filter :authenticate_user!, only: [ :index ]
 
-  def index
-    path = Rails.root.to_s + '/public/gallery/'
-    full = Dir.entries(path)
-    full.delete('.')
-    full.delete('..')
-    thumbs = Rails.root.to_s + '/public/gallery/.thumbnails/'
-    display = Rails.root.to_s + '/public/gallery/.display/'
-    @pictures = []
-    full.each do |file|
-      if File.file?(thumbs + file) && File.file?(display + file)
-        @pictures << file
-      end
-    end
-    @pictures.sort!.reverse!
-  end
+  def index; end
 end
