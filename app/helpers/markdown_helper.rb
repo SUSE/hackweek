b925fb566bac4798fc020100baa11cd2bf5bc6ad
Henne Vogelsang <hvogel@opensuse.org>
2017-02-21 15:26:40 +0100

    Introduce some save markdown rendering

--- app/helpers/markdown_helper.rb
+++
@@ -1,8 +0,0 @@
-module MarkdownHelper
-  def self.render markdown_source
-    renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer)
-
-    markdown.render markdown_source
-  end
-end
