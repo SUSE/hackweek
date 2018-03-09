f58e579e93898faef339b26346b387810006de35
Kaustubh Nair <kaustubh.nair108@gmail.com>
2018-03-09 11:51:55 +0530

    filter search by episode

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,15 @@ class SearchController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [ :result ]
   def result
     # First search in morphology mode, if fails — retry in wildcard mode
-    @projects = Project.search params[:q]
-    @projects = Project.search params[:q], star: true if @projects.empty?
+    episode = params[:episode]
+
+    if episode != "all"
+      @projects = Project.search params[:q],conditions: {episode_names: episode}
+      @projects = Project.search params[:q],conditions: {episode_names: episode}, star: true if @projects.empty?
+    else
+      @projects = Project.search params[:q]
+      @projects = Project.search params[:q], star: true if @projects.empty?
+    end
 
     @users = User.search params[:q]
     @users = User.search params[:q], star: true if @users.empty?
--- app/indices/project_index.rb
+++ app/indices/project_index.rb
@@ -2,10 +2,12 @@ ThinkingSphinx::Index.define :project, name: :project_real_time, with: :real_tim
   indexes title
   indexes description
   indexes comment_texts
+  indexes episode_names
 end
 
 ThinkingSphinx::Index.define :project, name: :project_active_record, with: :active_record do
   indexes title
   indexes description
   indexes comments.text, :as => :comments
+  indexes episodes.name, as: :episode_names
 end
