eca8083c739a5a5ea71a781d66bdf8a451eb7aa5
Kaustubh Nair <kaustubh.nair@iiitb.org>
2018-03-10 14:46:43 +0530

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,14 @@ class SearchController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [ :result ]
   def result
     # First search in morphology mode, if fails — retry in wildcard mode
-    episode = params[:episode]
-
-    if episode != "all"
-      @projects = Project.search params[:q],conditions: {episode_names: episode}
-      @projects = Project.search params[:q],conditions: {episode_names: episode}, star: true if @projects.empty?
-    else
+    
+    if params[:episode] == "all"
       @projects = Project.search params[:q]
       @projects = Project.search params[:q], star: true if @projects.empty?
+    else
+      @episode = Episode.find(params[:episode])
+      @projects = Project.search params[:q],conditions: {episode_names: @episode.name}
+      @projects = Project.search params[:q],conditions: {episode_names: @episode.name}, star: true if @projects.empty?
     end
 
     @users = User.search params[:q]
