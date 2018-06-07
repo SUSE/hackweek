0cfc1d9c0064c859a2203f1e3db35486e456e1fe
AnkushMalik <ankushmalik631@gmail.com>
2018-06-07 16:05:12 +0530

    Add test case for originator upvote bug

--- spec/features/collaboration_spec.rb
+++ spec/features/collaboration_spec.rb
@@ -52,4 +52,18 @@ feature 'Collaboration' do
     }.to change(Project.liked, :count).by(-1)
     expect(page).not_to have_css("dislike-#{project.id}")
   end
+
+  scenario 'User likes a project multiple times' do
+    user = create(:user)
+    project = create(:idea)
+    sign_in user
+
+    visit project_path(nil, project)
+    click_link "like-#{project.id}"
+
+    expect {
+      find("#like-#{project.id}", visible: false).click
+    }.to change(Project.liked, :count).by(0)
+    expect(page).not_to have_css("like-#{project.id}")
+  end
 end
