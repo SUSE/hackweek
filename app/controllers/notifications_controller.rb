4f70a2742d7fbee2bed2659898a5c5d799003bd1
AnkushMalik <ankushmalik631@gmail.com>
2018-07-20 21:23:13 +0530

    Add older notifications page

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.logged_in(current_user).unread
+    @older_notifications = Notification.logged_in(current_user).order('created_at DESC').page(params[:page])
   end
 
   def mark_as_read
