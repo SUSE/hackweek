f86e00a677becda59e7d5a19ba48578c6283bcf2
Henne Vogelsang <hvogel@opensuse.org>
2019-04-05 15:25:38 +0200

    Define association before using it in :through
    This is now an error.

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,8 +4,8 @@ class Announcement < ApplicationRecord
   validates :originator_id, presence: true
 
   belongs_to :originator, class_name: 'User'
-  has_many :users, through: :enrollments
   has_many :enrollments
+  has_many :users, through: :enrollments
 
   def enroll! user
     self.users << user
