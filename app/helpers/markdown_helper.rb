405993d4a6a6b89cb36073fe27b4e4cee5994e3d
Thomas Schmidt <tschmidt@suse.de>
2021-03-11 23:45:57 +0100

    Don't cut markdown preview after 140 chars, which can lead to invalid syntax. Cut after n lines instead and filter out comments and empty lines.

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,4 +2,8 @@ module MarkdownHelper
   def self.render(markdown_source)
     Haml::Filters::Markdown.render markdown_source
   end
+
+  def mdpreview(markdown_source, lines: 3)
+    markdown_source.lines.reject { |l| l =~ /\[comment\]/ }.select { |l| l =~ /\S/ }[0..lines].join
+  end
 end
