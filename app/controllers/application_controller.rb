c85a948303d9248ff30e8f56f493cd7b5f979917
Henne Vogelsang <hvogel@opensuse.org>
2022-06-03 16:56:06 +0200

    Make sure CSRF protection happens before anything else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
 
   before_action :store_location
   before_action :authenticate_user!
