a189c41a108b23c9eeefecc9129deb11e651e237
Dani Donisa <dani.doni@gmail.com>
2024-04-25 13:09:53 +0200

    Fix errors regarding wrong scopes for Rails.application
        NoMethodError:
            undefined method `application' for Haml::Rails:Module

--- lib/haml/filters/markdown.rb
+++ lib/haml/filters/markdown.rb
@@ -13,11 +13,11 @@ module Haml
       def render(text)
         # replace @user with a link to user
         text.gsub!(/([^\w]|^)@([-\w]+)([^\w]|$)/) do
-          "#{Regexp.last_match(1)}[@#{Regexp.last_match(2)}](#{Rails.application.routes.url_helpers(only_path: true).user_path(Regexp.last_match(2))})#{Regexp.last_match(3)}"
+          "#{Regexp.last_match(1)}[@#{Regexp.last_match(2)}](#{::Rails.application.routes.url_helpers(only_path: true).user_path(Regexp.last_match(2))})#{Regexp.last_match(3)}"
         end
         # replace hw#slug with a link to project
         text.gsub!(/([^\w]|^)hw#([-\w]+)([^\w]|$)/) do
-          "#{Regexp.last_match(1)}[hw##{Regexp.last_match(2)}](#{Rails.application.routes.url_helpers(only_path: true).project_path(Regexp.last_match(2))})#{Regexp.last_match(3)}"
+          "#{Regexp.last_match(1)}[hw##{Regexp.last_match(2)}](#{::Rails.application.routes.url_helpers(only_path: true).project_path(Regexp.last_match(2))})#{Regexp.last_match(3)}"
         end
 
         renderer_options = { escape_html: true,
