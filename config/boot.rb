f7d85797c7918646a39431d4945caf2fd76b6e52
Daniel Donisa <dani.doni@gmail.com>
2025-01-30 12:03:41 +0100

    Fix the missing logger explicit requirement

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+# FIXME: This is fixed in 7.1 (see https://github.com/rails/rails/issues/54260#issuecomment-2594962172)
+# Until we upgrade to 7.1 we need this hack in place
+require 'logger'
